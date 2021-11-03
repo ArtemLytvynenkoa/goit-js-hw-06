2e603aa0741e97c9e58b124875ad18963f7a4b27
Artem Lytvynenko <chpart355@gmail.com>
2021-11-03 17:47:10 +0200

    update task-2.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,28 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const recipeListEl = document.querySelector('#ingredients');
+
+// const itemsOfIngredients = ingredients.map(ingredient => {
+//   const itemEl = document.createElement('li');
+//   itemEl.textContent = ingredient;
+//   itemEl.classList.add('item');
+
+//   return itemEl;
+// })
+
+const makeItemsOfIngredients = array => {
+  return array.map(ingredient => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = ingredient;
+    itemEl.classList.add('item');
+
+    return itemEl;
+  });
+};
+
+const elements = makeItemsOfIngredients(ingredients);
+
+recipeListEl.append(...elements);
