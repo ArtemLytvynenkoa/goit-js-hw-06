9c84d62e2af22c9b2c47fbda6e86d902581bb381
Artem Lytvynenko <chpart355@gmail.com>
2021-11-07 21:28:47 +0200

    update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
+
+
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const email = event.currentTarget.elements.email;
+    const password = event.currentTarget.elements.password;
+    const personalData = { email, password };
+
+    personalData.email = event.currentTarget.elements.email.value;
+    personalData.password = event.currentTarget.elements.password.value;
+
+    if (email.value === '' || password.value === '') {
+        alert('All fields must be filled!!!');
+    } else {
+        console.log(personalData);
+    }
+
+    form.reset();
+};
+
+form.addEventListener('submit', onFormSubmit)
+
 
