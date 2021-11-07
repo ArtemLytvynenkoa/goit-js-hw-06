faf42ec6736e04fda851a0e553f8eeee2fa44c7b
Artem Lytvynenko <chpart355@gmail.com>
2021-11-07 23:39:24 +0200

    update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,12 @@ const inputEl = document.querySelector("#name-input");
 const spanEl = document.querySelector("#name-output");
 
 const onInputChanges = (event) => {
-    spanEl.textContent = event.currentTarget.value;
+    const textInInput = event.currentTarget.value;
+    spanEl.textContent = textInInput;
+
+    if (textInInput.length === 0) {
+        spanEl.textContent = 'Anonymous';
+    }
 };
 
 inputEl.addEventListener('input', onInputChanges);
