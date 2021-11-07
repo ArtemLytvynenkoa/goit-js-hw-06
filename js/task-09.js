d53d09a41f8685586a0dd321e5ce4f6c0ea6df98
Artem Lytvynenko <chpart355@gmail.com>
2021-11-07 21:39:17 +0200

    update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+
+const onBackgroundBodyClick = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = `${body.style.backgroundColor}`;
+};
+
+
+buttonEl.addEventListener('click', onBackgroundBodyClick);
