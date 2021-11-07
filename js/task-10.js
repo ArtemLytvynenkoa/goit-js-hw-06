3ecce25a4f9af40b5a6c92489e8b26884299b4aa
Artem Lytvynenko <chpart355@gmail.com>
2021-11-07 23:21:04 +0200

    update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const container = document.querySelector('#boxes');
+
+
+
+const onInputChange = () => {
+  return inputEl.value;
+};
+
+const onButtonCreateClick = () => {
+  const containerElements = [];
+  let boxWidth = 20;
+  let boxHeight = 20;
+
+  for (let i = 0; i < onInputChange(); i += 1) {
+    boxWidth += 10;
+    boxHeight += 10;
+    containerElements.push(`<div style = "background-color: ${getRandomHexColor()}; width: ${boxWidth}px; height: ${boxHeight}px"></div>`);
+  }
+
+  container.insertAdjacentHTML('afterbegin', containerElements.join(' '));
+
+};
+
+const onButtonDestroyClick = () => {
+  container.innerHTML = '';
+};
+
+inputEl.addEventListener('change', onInputChange);
+createButton.addEventListener('click', onButtonCreateClick);
+destroyButton.addEventListener('click', onButtonDestroyClick);
